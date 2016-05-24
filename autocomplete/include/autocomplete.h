c9f12962a386b6e26e5a0922455177c6c237cedf
zimmer_n <nicolas.zimmermann@epitech.eu>
2016-05-24 17:40:01 +0200

    [Autocomplete] > folder clean > ncurse starded > lib printf <compilation error>

--- autocomplete/include/autocomplete.h
+++ autocomplete/include/autocomplete.h
@@ -5,15 +5,17 @@
 ** Login   <zimmer_n@epitech.net>
 ** 
 ** Started on  Tue May 10 14:49:26 2016 Nicolas Zimmermann
-** Last update Tue May 10 17:05:29 2016 Nicolas Zimmermann
+** Last update Tue May 24 17:08:00 2016 Nicolas Zimmermann
 */
 
 #ifndef AUTOCOMPLETE_H_
 # define AUTOCOMPLETE_H_
 
 # include <dirent.h>
+# include <ncurses.h>
 
 # define NB_PATHES 7
+# define MAX_ELEM_PRINTABLE 10
 
 typedef	struct	s_file
 {
@@ -31,5 +33,7 @@ typedef struct 	s_autocomp
 void	list_file(t_autocomp *autoc);
 void	free_file_list(t_autocomp *autoc);
 void	show_file_list(t_autocomp *autoc);
+
+void	oh_my_42sh(t_autocomp *autoc);
 
 #endif /* !AUTOCOMPLETE_H_ */
---
+++ autocomplete/ncurse/OM42sh.c
@@ -0,0 +1,38 @@
+/*
+** OM42sh.c for 42sh in /home/zimmer_n/rendu/PSU_2015_42sh/autocomplete
+** 
+** Made by Nicolas Zimmermann
+** Login   <zimmer_n@epitech.net>
+** 
+** Started on  Tue May 24 16:55:24 2016 Nicolas Zimmermann
+** Last update Tue May 24 17:15:06 2016 Nicolas Zimmermann
+*/
+
+#include <unistd.h>
+#include "autocomplete.h"
+#include "my_printf.h"
+
+int	check_nb_elem(t_autocomp *autoc)
+{
+  char	c;
+
+  if (autoc->nb_elem > MAX_ELEM_PRINTABLE)
+    {
+      my_printf("Autocomplete found %d elements beggining by %s\n", autoc->nb_elem, autoc->buf);
+      while (42)
+	{
+	  my_printf("Do you want to display all ? <y/n>\n");
+	  read(1, &c, 1);
+	  if (c == 'y' || c == 'n')
+	    return ((c == 'y') ? 0 : 1);
+	}
+    }
+  return (0);
+}
+
+void	oh_my_42sh(t_autocomp *autoc)
+{
+  if (check_nb_elem(autoc) == 1)
+    return ;
+  
+}
